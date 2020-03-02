0ac0982da0d503057f446a208977acd0ee32bc57
Jonathan Mutakura <jmutakura@gmail.com>
2020-03-01 20:05:06 -0600

    Done with Principal field and the interest field

--- calc.py
+++ calc.py
@@ -1,11 +1,41 @@
-import tkinter as tk
+from tkinter import *
 
-app = tk.Tk()
+# Window oif the ap
+app = Tk()
 app.title('Compound interest calculator')
 app.geometry('700x700')
+
+# UI
+# principal
+principal_text = StringVar()
+principal_label = Label(app, text="Principal Amount", font=('bold', 14), pady=20)
+principal_label.grid(row=0, column=0, sticky=W)
+principal_entry = Entry(app, textvariable=principal_text)
+principal_entry.grid(row=0, column=1)
+
+# Interest rate
+interest_text = StringVar()
+interest_label = Label(app, text="Interest rate", font=('bold', 14), pady=20)
+interest_label.grid(row=1, column=0, sticky=W)
+interest_entry = Entry(app, textvariable=interest_text)
+interest_entry.grid(row=1, column=1)
+
 
 
 
 
 # Starting the program
 app.mainloop()
+
+
+
+
+
+
+# Equation for calculating interest rates
+principal = 0
+interest_rate = 0
+compounding_frequency = 0
+time = 0
+
+interest_amount = principal * (1 + (interest_rate / compounding_frequency)) ** (compounding_frequency * time)
